9564e22e4e0abc34466b0d03a27c18fbf17232fa
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-05-28 18:24:45 +0300

    change styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import { NavLink } from "react-router-dom";
 
 export const Header = () => {
     return (
-        <nav className="navbar bg-dark mb-3 navbar-expand-lg">
+        <nav className="navbar  mb-3 navbar-expand-lg" style={{backgroundColor: "grey"}}>
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"></button>
                     <div className="navbar-nav">
-                        <NavLink className="nav-link text-white"  aria-current="page" to="/">Home</NavLink>
-                        <NavLink className="nav-link text-white"  to="/movie">Movies</NavLink>
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="/movie">Movies</NavLink>
                     </div>
                 </div>
             </div>
